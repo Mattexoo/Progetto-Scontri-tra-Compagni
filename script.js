d6f30dda541d5f5e4b9d9f6fd587d526821d3410
pitPereno <pietropereno06@gmail.com>
2023-04-15 10:49:41 +0200

    risoluzione problemi

--- script.js
+++ script.js
@@ -10,33 +10,44 @@ let f = document.getElementById("img6");
 let g = document.getElementById("img7");
 let h = document.getElementById("img8");
 
+let personaggio1 =0;
+let personaggio2=0;
+
 //scelta personaggio giocatore 1
 a.addEventListener("click", function(){
-    let personaggio1 = a;
+     personaggio1 = "a";
+     console.log(personaggio1);
 });
 b.addEventListener("click", function(){
-    let personaggio1 = b;
+     personaggio1 = "b";
+     console.log(personaggio1);
 });
 c.addEventListener("click", function(){
-    let personaggio1 = c;
+     personaggio1 = "c";
+     console.log(personaggio1);
 });
 d.addEventListener("click", function(){
-    let personaggio1 = d;
+     personaggio1 = "d";
+     console.log(personaggio1);
 });
 
 //scelta personaggio giocatore 2
 e.addEventListener("click", function(){
-    let personaggio2 = e;
+     personaggio2 = "e";
+     console.log(personaggio2);
 });
 f.addEventListener("click", function(){
-    let personaggio2 = f;
+     personaggio2 = "f";
+     console.log(personaggio2);
 });
 g.addEventListener("click", function(){
-    let personaggio2 = f;
+     personaggio2 = "g";
+     console.log(personaggio2);
 });
 h.addEventListener("click", function(){
-    let personaggio2 = h;
+     personaggio2 = "h";
+     console.log(personaggio2);
 });
 
-console.log(personaggio1)
-console.log(personaggio2)
+console.log(personaggio1);
+console.log(personaggio2);
